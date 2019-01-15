f435906f77be61b887d9250e3ed65a8b11c7e38d
aabouriah <aabouriah@egypt.itgsolutions.com>
2019-01-15 18:15:16 +0200

    add adMob and firebase to IOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,14 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-
+@import GoogleMobileAds;
 @implementation AppDelegate
-
+    
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    [GADMobileAds configureWithApplicationID:@"ca-app-pub-4790983019049317~4474414942"];
+    return YES;
+    
 }
-
+    
 @end
